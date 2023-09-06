82d9aa069864a8024a2dd821586319ace964e4e0
shobhitrastogi <rshobhit255@gmail.com>
2023-09-06 18:24:15 +0530

    shopper-first commit
    first commit shobhit rastogi

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductList from './pages/ProductList';
+import Cart from './pages/Cart';
+import ProductDetail from './pages/ProductDetail';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Blog from './pages/Blog';
+
+
+const App = () => {
+  // Initialize the cart items as an empty array in the App component's state
+  const [cartItems, setCartItems] = useState([]);
+  
+  // Function to add an item to the cart
+  const addItemToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  // Function to remove an item from the cart
+  const removeItemFromCart = (index) => {
+    const updatedCart = [...cartItems];
+    updatedCart.splice(index, 1);
+    setCartItems(updatedCart);
+  };
+
+  return (
+    <BrowserRouter>
+       <Navbar/>
+      <Routes>
+        <Route
+          path="/"
+          element={<ProductList addItemToCart={addItemToCart} />}
+        />
+        <Route
+          path="/cart"
+          element={<Cart cartItems={cartItems} removeItemFromCart={removeItemFromCart} />}
+        />
+        <Route
+          path="/product/:id"
+          element={<ProductDetail addItemToCart={addItemToCart} />}
+        />
+        <Route
+          path="/about"
+          element={<Blog />}
+        />
+        
+      </Routes>
+      <Contact />
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const ProductList = ({ addItemToCart }) => {
+  const [data, setData] = useState([]);
+  const api = 'https://dummyjson.com/products';
+
+  // Initialize the history object
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData.products);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Function to add an item to the cart and navigate to the cart page
+  const addItemToCartAndNavigate = (product) => {
+    addItemToCart(product);
+    // Navigate to the cart page
+    navigate('/cart');
+  };
+
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-3 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {data.map((product) => (
+            <div className="lg:w-1/4 md:w-2/2 p-4 w-full " key={product.id}>
+              <Link to={`/product/${product.id}`} className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt={`Product ${product.id}`}
+                  className="object-cover object-center w-full h-full block"
+                  src={product.images[0]} // Use the first image as the src
+                />
+              </Link>
+              <div className="mt-4">
+                
+                <h2 className="text-gray-900 title-font text-lg font-medium">{product.title}</h2>
+                <p className="mt-1">${product.price.toFixed(2)}</p>
+                <div className="flex items-center flex-wrap">
+                  <a
+                    className="text-dark-500 inline-flex items-center md:mb-2 lg:mb-0"
+                    onClick={() => addItemToCartAndNavigate(product)} // Add to cart and navigate
+                  >
+                    Buy Now
+                    <svg
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                   <a href={`/product/${product.id}`}
+                    className="text-dark-500 inline-flex items-center md:mb-2 lg:mb-0"
+                     // Add to cart and navigate
+                  >
+                    view detail 
+                    <svg
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductList;
