b878910aeb14e447f25f7a99a90b317a11ab17a1
shobhitrastogi <rshobhit255@gmail.com>
2023-10-02 20:58:37 +0530

    Some errors

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-// App.js
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-=======
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
->>>>>>> 82d9aa069864a8024a2dd821586319ace964e4e0
 import ProductList from './pages/ProductList';
 import Cart from './pages/Cart';
 import ProductDetail from './pages/ProductDetail';
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Blog from './pages/Blog';
-<<<<<<< HEAD
 import Login from './components/Login';
 import Signup from './components/Signup';
-=======
->>>>>>> 82d9aa069864a8024a2dd821586319ace964e4e0
-
 
 const App = () => {
   // Initialize the cart items as an empty array in the App component's state
   const [cartItems, setCartItems] = useState([]);
-<<<<<<< HEAD
 
   // Function to add an item to the cart
   const addItemToCart = (product) => {
     setCartItems([...cartItems, product]);
-    
-=======
-  
-  // Function to add an item to the cart
-  const addItemToCart = (product) => {
-    setCartItems([...cartItems, product]);
->>>>>>> 82d9aa069864a8024a2dd821586319ace964e4e0
   };
 
   // Function to remove an item from the cart
@@ -40,27 +23,15 @@ const App = () => {
     const updatedCart = [...cartItems];
     updatedCart.splice(index, 1);
     setCartItems(updatedCart);
-<<<<<<< HEAD
- 
-=======
->>>>>>> 82d9aa069864a8024a2dd821586319ace964e4e0
   };
 
   return (
     <BrowserRouter>
-       <Navbar/>
+      <Navbar />
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route
-<<<<<<< HEAD
-          path="/login"
-          element={<Login/>}
-        />  <Route
-          path="/signup"
-          element={<Signup/>}
-        />
-        <Route
-=======
->>>>>>> 82d9aa069864a8024a2dd821586319ace964e4e0
           path="/"
           element={<ProductList addItemToCart={addItemToCart} />}
         />
@@ -72,11 +43,7 @@ const App = () => {
           path="/product/:id"
           element={<ProductDetail addItemToCart={addItemToCart} />}
         />
-        <Route
-          path="/about"
-          element={<Blog />}
-        />
-        
+        <Route path="/about" element={<Blog />} />
       </Routes>
       <Contact />
     </BrowserRouter>
